b4fc5932b08146396d33cd3c5591f1cac78526da
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-07-05 15:52:25 +0530

    GJ212P-8 Basic Folder Structure Created

--- FrontEnd/blazerrentalmgt/src/App.js
+++ FrontEnd/blazerrentalmgt/src/App.js
@@ -1,9 +1,17 @@
+
 import './App.css';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from './login/login';
 
 function App() {
   return (
     <div className="App">
-      <h1> Blazer</h1>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Login/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
